c9ecb6df88d93c82bea92cc7266d3e8711b44d9d
whoopsjohnnie <jgrundback@gmail.com>
2026-06-22 16:07:43 -0700

    Chat work

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -5,6 +5,9 @@ import { parseSSEStream } from '../utils/utils';
 import ChatMessages from './ChatMessages';
 import ChatInput from './ChatInput';
 
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+
 function Chatbot() {
   const [chatId, setChatId] = useState(null);
   const [messages, setMessages] = useImmer([]);
@@ -50,13 +53,23 @@ function Chatbot() {
 
   return (
     <div className='relative grow flex flex-col gap-6 pt-6'>
-      {messages.length === 0 && (
-        <div className='mt-3 font-urbanist text-primary-blue text-xl font-light space-y-2'>
+      {/* {messages.length === 0 && ( */}
+        {/* <div className='mt-3 font-urbanist text-primary-blue text-xl font-light space-y-2'>
           <p>👋 Welcome!</p>
           <p>I am powered by the latest technology reports from leading institutions like the World Bank, the World Economic Forum, McKinsey, Deloitte and the OECD.</p>
           <p>Ask me anything about the latest technology trends.</p>
-        </div>
-      )}
+        </div> */}
+        <Alert icon={
+          false
+        } 
+          severity="success">
+          <AlertTitle>
+            <p>👋 Welcome!</p>
+          </AlertTitle>
+          <p>I am powered by the latest technology reports from leading institutions like the World Bank, the World Economic Forum, McKinsey, Deloitte and the OECD.</p>
+          <p>Ask me anything about the latest technology trends.</p>
+        </Alert>
+      {/* )} */}
       <ChatMessages
         messages={messages}
         isLoading={isLoading}
